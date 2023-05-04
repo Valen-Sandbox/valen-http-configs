73567b57ef6d8e0485ebb427ebc7ac771d968f63
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-05-04 11:24:00 -0400

    Create valen_http_configs.lua

---
+++ lua/cfc_http_restrictions/configs/valen_http_configs.lua
@@ -0,0 +1,30 @@
+AddCSLuaFile()
+
+return {
+    version = "1", -- this field allows backwards compatibility if the config structure is ever updated
+
+    wrapHTMLPanels = true,
+
+    addresses = {
+        -- domains starfall docs have
+        ["npms.io"] = { allowed = true },
+        ["fb.me"] = { allowed = true },
+        ["reactjs.org"] = { allowed = true },
+        ["www.w3.org"] = { allowed = true },
+
+        ["google.com"] = { allowed = true, permanent = true },
+        ["www.google.com"] = { allowed = true, permanent = true },
+
+        ["garrysmod.site"] = { allowed = true, permanent = true, noisy = true },
+        ["*.garrysmod.site"] = { allowed = true, permanent = true, noisy = true },
+
+        ["*.pointshop2.com"] = { allowed = true, permanent = true },
+
+        ["discord.gg"] = { allowed = true },
+        ["cdn[%w-_]*.discordapp%.com"] = { allowed = true },
+        ["images-([%w%-]+)%.discordapp%.net"] = { allowed = true },
+
+        -- media player
+        ["samuelmaddock.github.io"] = { allowed = true }
+    }
+}
